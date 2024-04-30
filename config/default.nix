df941348efcaefafd77000e146cd53b2f6f54ddb
Kevin Yokley <kyokley2@gmail.com>
2024-04-30 16:38:42 -0500

    Fix escaping

--- config/default.nix
+++ config/default.nix
@@ -181,17 +181,18 @@
     }
     {
         key = "<leader>gb";
-        action = '':<C-U>tabnew | terminal git blame <C-R>=expand("%:p") <CR> | color_git_blame | less +<C-R>=max([0, line(\".\") - winline()]) <CR><CR><CR>'';
+        action = '':<C-U>tabnew | terminal git blame <C-R>=expand("%:p") <CR> | color_git_blame | less +<C-R>=max([0, line('.') - winline()]) <CR><CR><CR>'';
         mode = ["n"];
     }
     {
         key = "<leader>gb";
-        action = '':<C-U>tabnew | terminal git blame <C-R>=expand("%:p") <CR> | sed -n <C-R>=line("\"<") <CR>,<C-R>=line("\">") <CR>p | color_git_blame | less <CR><CR>'';
+        action = '':<C-U>tabnew | terminal git blame <C-R>=expand("%:p") <CR> | sed -n <C-R>=line("'<") <CR>,<C-R>=line("'>") <CR>p | color_git_blame | less <CR><CR>'';
         mode = ["v"];
     }
     {
         key = "<leader>gl";
-        action = '':<C-U>tabnew | terminal git blame <C-R>=expand("%:p") <CR> | sed -n <C-R>=line(".") <CR>p | awk \"{print $1}\" | tr -d \"^\" | xargs git show <CR><CR>'';
+        action = '':<C-U>tabnew | terminal git blame <C-R>=expand("%:p") <CR> | sed -n <C-R>=line(".") <CR>p | awk '{print $1}' | tr -d '^' | xargs git show <CR><CR>'';
+        mode = ["n"];
     }
   ];
 
