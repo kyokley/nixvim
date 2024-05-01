ab9aff828e41f3bac4345cee038337eb027ea723
Kevin Yokley <kyokley2@gmail.com>
2024-05-01 07:09:42 -0500

    Fix config

--- config/bufferline.nix
+++ config/bufferline.nix
@@ -1,10 +1,16 @@
 {
   plugins.bufferline = {
     enable = true;
-    diagnostics = "nvim_lsp";
     numbers = "buffer_id";
     separatorStyle = "slant";
     showBufferCloseIcons = false;
     enforceRegularTabs = true;
+    diagnostics = "nvim_lsp";
+    diagnosticsIndicator = ''
+    function(count, level, diagnostics_dict, context)
+      local icon = level:match("error") and " " or " "
+      return " " .. icon .. count
+    end
+    '';
   };
 }
--- config/default.nix
+++ config/default.nix
@@ -52,53 +52,75 @@
       "termguicolors" = true;
   };
 
-  highlight = {
-    EndOfBuffer.ctermbg = null;
+  highlightOverride = {
+    EndOfBuffer = {
+        ctermbg = null;
+        bg = null;
+    };
     # MatchParen.ctermbg = "4";
 
-    # Normal.ctermbg = null;
-    # NonText.ctermbg = null;
-
-    CursorLine = {
-        # cterm = null;
-        ctermbg = "blue";
-        ctermfg = "white";
+    Normal = {
+        ctermbg = "black";
+        bg = "black";
+    };
+    NormalNC = {
+        ctermbg = "black";
+        bg = "black";
+    };
+    NonText = {
+        ctermbg = null;
+        bg = null;
     };
 
-    # ColorColumn.ctermbg = "black";
+    CursorLine = {
+        ctermbg = "darkblue";
+        ctermfg = "white";
+        bg = "darkblue";
+        fg = "white";
+    };
+
+    ColorColumn = {
+        ctermbg = "black";
+        bg = "black";
+    };
+
     TabLineFill = {
         cterm = null;
         ctermbg = null;
-        guibg = null;
+        bg = null;
     };
 
     TabLine = {
         cterm = null;
         ctermbg = null;
-        guibg = null;
+        bg = null;
     };
 
     search = {
         cterm = null;
         ctermbg = "lightblue";
         ctermfg = "black";
+        bg = "lightblue";
+        fg = "black";
     };
 
     signcolumn = {
         cterm = null;
         ctermbg = "black";
+        bg = "black";
     };
-    # visual = {
-    #     cterm = null;
-    #     ctermbg = "white";
-    #     ctermfg = "black";
-    # };
+    visual = {
+        cterm = null;
+        ctermbg = "darkgrey";
+        bg = "darkgrey";
+        # ctermfg = "black";
+    };
 
-    # statusline ={
-    #     cterm = null;
-    #     # ctermbg = "4";
-    #     ctermfg = "white";
-    # };
+     statusline ={
+         cterm = null;
+         # ctermbg = "4";
+         ctermfg = "white";
+     };
 
   };
 
@@ -265,105 +287,105 @@
     {
       event = ["InsertEnter"];
       pattern = "*";
-      command = ''if &buftype != 'nofile' | highlight LineNr ctermbg=darkred | endif'';
+      command = ''if &buftype != 'nofile' | highlight LineNr ctermbg=darkred guibg=darkred | endif'';
       group = "general_setup";
     }
     {
       event = ["InsertEnter"];
       pattern = "*";
-      command = ''if &buftype != 'nofile' && &buftype != 'prompt' | highlight CursorLine ctermbg=darkred | endif'';
+      command = ''if &buftype != 'nofile' && &buftype != 'prompt' | highlight CursorLine ctermbg=darkred guibg=darkred | endif'';
       group = "general_setup";
     }
     {
         event = ["InsertEnter"];
         pattern = "*";
-        command = ''if &buftype != 'nofile' | highlight statusline ctermbg=darkred | endif'';
+        command = ''if &buftype != 'nofile' | highlight statusline ctermbg=darkred guibg=darkred | endif'';
         group = "general_setup";
     }
     {
       event = ["InsertLeave"];
       pattern = "*";
-      command = ''if &buftype != 'nofile' | highlight LineNr ctermbg=NONE | endif'';
+      command = ''if &buftype != 'nofile' | highlight LineNr ctermbg=NONE guibg=NONE | endif'';
       group = "general_setup";
     }
     {
       event = ["InsertLeave"];
       pattern = "*";
-      command = ''if &buftype != 'nofile' && &buftype != 'prompt' | highlight CursorLine ctermbg=darkblue | endif'';
+      command = ''if &buftype != 'nofile' && &buftype != 'prompt' | highlight CursorLine ctermbg=darkblue guibg=darkblue | endif'';
       group = "general_setup";
     }
     {
       event = ["InsertLeave"];
       pattern = "*";
-      command = ''if &buftype != 'nofile' | highlight statusline ctermbg=darkblue guibg=#203780 | endif'';
+      command = ''if &buftype != 'nofile' | highlight statusline ctermbg=darkblue guibg=darkblue | endif'';
       group = "general_setup";
     }
-    # {
-    #   event = ["BufReadPost"];
-    #   pattern = "*";
-    #   command = ''if line(\"'\\\"\") > 0|if line(\"'\\\"\") <= line(\"$\")|exe(\"norm '\\\"\")|else|exe \"norm $\"|endif|endif'';
-    #   group = "general_setup";
-    # }
-    # {
-    #   event = ["VimEnter"];
-    #   pattern = "*";
-    #   command = ''if &filetype != 'gitcommit' | highlight ExtraWhitespace ctermbg=darkred ctermfg=yellow | endif'';
-    #   group = "general_setup";
-    # }
-    # {
-    #   event = ["VimEnter"];
-    #   pattern = "*";
-    #   command = ''if &filetype != 'gitcommit' | match ExtraWhitespace /\\s\\+$\\|\\t/ | endif'';
-    #   group = "general_setup";
-    # }
-    # {
-    #   event = ["BufEnter"];
-    #   pattern = "*";
-    #   command = ''let &titlestring = "nvim " . expand("%:p")'';
-    #   group = "general_setup";
-    # }
-    # {
-    #   event = ["FocusGained" "VimEnter" "WinEnter" "BufWinEnter"];
-    #   pattern = "*";
-    #   command = ''setlocal cursorline'';
-    #   group = "general_setup";
-    # }
-    # {
-    #   event = ["FocusLost" "WinLeave"];
-    #   pattern = "*";
-    #   command = ''setlocal nocursorline'';
-    #   group = "general_setup";
-    # }
-    # {
-    #   event = ["FocusGained"];
-    #   pattern = "*";
-    #   command = ''checktime'';
-    #   group = "general_setup";
-    # }
-    # {
-    #   event = ["TermClose"];
-    #   pattern = "<buffer>";
-    #   command = ''if &buftype == 'terminal' | bdelete! | endif'';
-    #   group = "general_setup";
-    # }
-    # {
-    #   event = ["TermOpen"];
-    #   pattern = "*";
-    #   command = ''setlocal nonumber norelativenumber bufhidden=hide'';
-    #   group = "terminal_setup";
-    # }
-    # {
-    #   event = ["TermOpen" "BufWinEnter" "WinEnter"];
-    #   pattern = "term://*";
-    #   command = ''startinsert'';
-    #   group = "terminal_setup";
-    # }
-    # {
-    #   event = ["BufLeave"];
-    #   pattern = "term://*";
-    #   command = ''stopinsert'';
-    #   group = "terminal_setup";
-    # }
+    {
+      event = ["BufReadPost"];
+      pattern = "*";
+      command = ''if line("'\\\"") > 0|if line("'\\\"") <= line("$")|exe("norm '\\\"")|else|exe "norm $"|endif|endif'';
+      group = "general_setup";
+    }
+    {
+      event = ["VimEnter"];
+      pattern = "*";
+      command = ''if &filetype != 'gitcommit' | highlight ExtraWhitespace ctermbg=darkred ctermfg=yellow guibg=darkred guifg=yellow | endif'';
+      group = "general_setup";
+    }
+    {
+      event = ["VimEnter"];
+      pattern = "*";
+      command = ''if &filetype != 'gitcommit' | match ExtraWhitespace /\\s\\+$\\|\\t/ | endif'';
+      group = "general_setup";
+    }
+    {
+      event = ["BufEnter"];
+      pattern = "*";
+      command = ''let &titlestring = "nvim " . expand("%:p")'';
+      group = "general_setup";
+    }
+    {
+      event = ["FocusGained" "VimEnter" "WinEnter" "BufWinEnter"];
+      pattern = "*";
+      command = ''setlocal cursorline'';
+      group = "general_setup";
+    }
+    {
+      event = ["FocusLost" "WinLeave"];
+      pattern = "*";
+      command = ''setlocal nocursorline'';
+      group = "general_setup";
+    }
+    {
+      event = ["FocusGained"];
+      pattern = "*";
+      command = ''checktime'';
+      group = "general_setup";
+    }
+    {
+      event = ["TermClose"];
+      pattern = "<buffer>";
+      command = ''if &buftype == 'terminal' | bdelete! | endif'';
+      group = "general_setup";
+    }
+    {
+      event = ["TermOpen"];
+      pattern = "*";
+      command = ''setlocal nonumber norelativenumber bufhidden=hide'';
+      group = "terminal_setup";
+    }
+    {
+      event = ["TermOpen" "BufWinEnter" "WinEnter"];
+      pattern = "term://*";
+      command = ''startinsert'';
+      group = "terminal_setup";
+    }
+    {
+      event = ["BufLeave"];
+      pattern = "term://*";
+      command = ''stopinsert'';
+      group = "terminal_setup";
+    }
   ];
 
   plugins = {
@@ -374,18 +396,6 @@
 
     nvim-tree = {
         enable = true;
-        # onAttach = {
-        #     __raw = ''
-        #         function(bufnr)
-        #             local api = require("nvim-tree.api")
-        #             vim.keymap.set(
-        #                 "n",
-        #                 "<F3>",
-        #                 function()
-        #                     local node = api.tree.get_node_under_cursor()
-        #                     print(node.absolute_path)
-        #                 end, { buffer = bufnr, noremap = true, silent = true, nowait = true, desc = "print the node's absolute path" }) end
-        #     '';};
     };
     telescope = {
         enable = true;
