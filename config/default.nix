586164ac759e511971ba528d775e03ec8434f8cc
Kevin Yokley <kyokley2@gmail.com>
2024-04-30 19:40:05 -0500

    Colorscheme changes

--- config/default.nix
+++ config/default.nix
@@ -52,6 +52,56 @@
       "termguicolors" = true;
   };
 
+  highlight = {
+    EndOfBuffer.ctermbg = null;
+    # MatchParen.ctermbg = "4";
+
+    # Normal.ctermbg = null;
+    # NonText.ctermbg = null;
+
+    CursorLine = {
+        cterm = null;
+        # ctermbg = "18";
+        ctermfg = "white";
+    };
+
+    ColorColumn.ctermbg = "black";
+    TabLineFill = {
+        cterm = null;
+        ctermbg = null;
+        guibg = null;
+    };
+
+    TabLine = {
+        cterm = null;
+        ctermbg = null;
+        guibg = null;
+    };
+
+    search = {
+        cterm = null;
+        ctermbg = "lightblue";
+        ctermfg = "black";
+    };
+
+    signcolumn = {
+        cterm = null;
+        ctermbg = "black";
+    };
+    visual = {
+        cterm = null;
+        ctermbg = "white";
+        ctermfg = "black";
+    };
+
+    statusline ={
+        cterm = null;
+        # ctermbg = "4";
+        ctermfg = "white";
+    };
+
+  };
+
   match = {
     ErrorMsg = ''^\(<\|=\|>\)\{7\}\([^=].\+\)\?$'';
   };
@@ -237,6 +287,72 @@
       pattern = "*";
       command = ''if &buftype != 'nofile' | highlight statusline ctermbg=darkblue guibg=#203780 | endif'';
       group = "general_setup";
+    }
+    {
+      event = ["BufReadPost"];
+      pattern = "*";
+      command = ''if line(\"'\\\"\") > 0|if line(\"'\\\"\") <= line(\"$\")|exe(\"norm '\\\"\")|else|exe \"norm $\"|endif|endif'';
+      group = "general_setup";
+    }
+    {
+      event = ["VimEnter"];
+      pattern = "*";
+      command = ''if &filetype != 'gitcommit' | highlight ExtraWhitespace ctermbg=darkred guibg=darkred ctermfg=yellow guifg=yellow | endif'';
+      group = "general_setup";
+    }
+    {
+      event = ["VimEnter"];
+      pattern = "*";
+      command = ''if &filetype != 'gitcommit' | match ExtraWhitespace /\\s\\+$\\|\\t/ | endif'';
+      group = "general_setup";
+    }
+    {
+      event = ["BufEnter"];
+      pattern = "*";
+      command = ''let &titlestring = "nvim " . expand("%:p")'';
+      group = "general_setup";
+    }
+    {
+      event = ["FocusGained" "VimEnter" "WinEnter" "BufWinEnter"];
+      pattern = "*";
+      command = ''setlocal cursorline'';
+      group = "general_setup";
+    }
+    {
+      event = ["FocusLost" "WinLeave"];
+      pattern = "*";
+      command = ''setlocal nocursorline'';
+      group = "general_setup";
+    }
+    {
+      event = ["FocusGained"];
+      pattern = "*";
+      command = ''checktime'';
+      group = "general_setup";
+    }
+    {
+      event = ["TermClose"];
+      pattern = "<buffer>";
+      command = ''if &buftype == 'terminal' | bdelete! | endif'';
+      group = "general_setup";
+    }
+    {
+      event = ["TermOpen"];
+      pattern = "*";
+      command = ''setlocal nonumber norelativenumber bufhidden=hide'';
+      group = "terminal_setup";
+    }
+    {
+      event = ["TermOpen" "BufWinEnter" "WinEnter"];
+      pattern = "term://*";
+      command = ''startinsert'';
+      group = "terminal_setup";
+    }
+    {
+      event = ["BufLeave"];
+      pattern = "term://*";
+      command = ''stopinsert'';
+      group = "terminal_setup";
     }
   ];
 
