c1b85c818564c6a1f6a44823ee0f0c8883b86e7d
Kevin Yokley <kyokley2@gmail.com>
2024-05-02 14:42:58 -0500

    Add barbar config

---
+++ config/barbar.nix
@@ -0,0 +1,11 @@
+{
+    plugins.barbar = {
+        enable = true;
+        clickable = false;
+        extraOptions = {
+            icons = {
+                buffer_index = true;
+            };
+        };
+    };
+}
