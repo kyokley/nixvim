5e59ef2ff3154ae2ceb16fd8936d7a9b8690c234
Kevin Yokley <kyokley2@gmail.com>
2024-05-05 21:35:45 -0500

    Add more LSPs

--- config/lsp.nix
+++ config/lsp.nix
@@ -3,6 +3,10 @@
         lsp = {
             enable = true;
             servers = {
+                bashls.enable = true;
+                dockerls.enable = true;
+                docker-compose-language-service.enable = true;
+                nginx-language-server.enable = true;
                 lua-ls.enable = true;
                 ruff.enable = true;
                 pylsp.enable = true;
