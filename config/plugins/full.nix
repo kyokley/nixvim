69cb2273d9e38934425d80a64fdaff4b7e660ef8
Kevin Yokley <kyokley2@gmail.com>
2024-06-28 11:35:33 -0500

    Allow telescope to open multiple files

--- config/plugins/full.nix
+++ config/plugins/full.nix
@@ -34,7 +34,7 @@ in
             ];
 
             settings.mapping = {
-                "<CR>" = "cmp.mapping.confirm({ select = true })";
+                "<CR>" = "cmp.mapping.confirm({ select = false })";
                 "<Tab>" = ''
                     cmp.mapping(
                             function(fallback)
@@ -89,6 +89,33 @@ in
             -- Vim-Mundo {{{
             vim.g.mundo_preview_bottom = 1
             -- vim.g.mundo_close_on_revert = 1
+            -- }}}
+
+            -- {{{ Telescope Config
+            local select_one_or_multi = function(prompt_bufnr)
+              local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+              local multi = picker:get_multi_selection()
+              if not vim.tbl_isempty(multi) then
+                require('telescope.actions').close(prompt_bufnr)
+                for _, j in pairs(multi) do
+                  if j.path ~= nil then
+                    vim.cmd(string.format('%s %s', 'edit', j.path))
+                  end
+                end
+              else
+                require('telescope.actions').select_default(prompt_bufnr)
+              end
+            end
+
+            require('telescope').setup {
+              defaults = {
+                mappings = {
+                  i = {
+                    ['<CR>'] = select_one_or_multi,
+                  }
+                }
+              }
+            }
             -- }}}
     '';
 }
