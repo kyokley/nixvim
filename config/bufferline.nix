171e6a3986030c0fbb85d315e0da7543388c1722
Kevin Yokley <kyokley2@gmail.com>
2024-09-01 23:36:56 -0500

    Update field names

--- config/bufferline.nix
+++ config/bufferline.nix
@@ -1,17 +1,19 @@
 {
   plugins.bufferline = {
     enable = true;
-    themable = true;
-    numbers = "buffer_id";
-    separatorStyle = "thick";
-    showBufferCloseIcons = false;
-    enforceRegularTabs = true;
-    diagnostics = "nvim_lsp";
-    diagnosticsIndicator = ''
-    function(count, level, diagnostics_dict, context)
-      local icon = level:match("error") and " " or " "
-      return " " .. icon .. count
-    end
-    '';
+    settings.options = {
+      themable = true;
+      numbers = "buffer_id";
+      separatorStyle = "thick";
+      showBufferCloseIcons = false;
+      enforceRegularTabs = true;
+      diagnostics = "nvim_lsp";
+      diagnosticsIndicator = ''
+      function(count, level, diagnostics_dict, context)
+        local icon = level:match("error") and " " or " "
+        return " " .. icon .. count
+      end
+      '';
+    };
   };
 }
