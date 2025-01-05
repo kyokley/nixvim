0924dae32d1f7f57fd6f1224be6184f3a5202764
Kevin Yokley <kyokley2@gmail.com>
2025-01-04 21:44:30 -0600

    Upgrade plugins

--- config/plugins/minimal.nix
+++ config/plugins/minimal.nix
@@ -58,7 +58,7 @@ in
     };
 
     extraPlugins = [
-        (fromGitHub "32929480b1753a5c2a99435e891da9be1e61e0b9" "main" "willothy/nvim-cokeline")
+        (fromGitHub "adfd1eb87e0804b6b86126e03611db6f62bb2909" "main" "willothy/nvim-cokeline")
     ];
 
     extraConfigLua = ''
