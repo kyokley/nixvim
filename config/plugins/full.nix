930e66c2909b9ea1fddeab759efc01226a5796f0
Kevin Yokley <kyokley@mars>
2025-07-28 20:44:23 -0500

    Set origami lua config

--- config/plugins/full.nix
+++ config/plugins/full.nix
@@ -93,7 +93,34 @@
       enable = true;
       enhancedDiffHl = true;
     };
-    origami.enable = true;
+    origami = {
+      enable = true;
+      luaConfig.post = ''
+        -- default settings
+        require("origami").setup {
+            useLspFoldsWithTreesitterFallback = true,
+            pauseFoldsOnSearch = true,
+            foldtext = {
+                enabled = true,
+                padding = 3,
+                lineCount = {
+                    template = "⤢ %d lines", -- `%d` is replaced with the number of folded lines
+                    hlgroup = "Comment",
+                },
+                diagnosticsCount = true, -- uses hlgroups and icons from `vim.diagnostic.config().signs`
+                gitsignsCount = true, -- requires `gitsigns.nvim`
+            },
+            autoFold = {
+                enabled = true,
+                kinds = { "comment", "imports" }, ---@type lsp.FoldingRangeKind[]
+            },
+            foldKeymaps = {
+                setup = true, -- modifies `h` and `l`
+                hOnlyOpensOnFirstColumn = false,
+            },
+        }
+      '';
+    };
   };
 
   extraPlugins = with pkgs.vimPlugins; [
