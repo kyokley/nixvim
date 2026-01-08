58c44757c25c6f591e31ea540baa05f634826c54
Kevin Yokley <kevin.yokley@oracle.com>
2026-01-08 17:00:17 -0600

    Remap zR and zM for ufo

--- config/plugins/full.nix
+++ config/plugins/full.nix
@@ -127,6 +127,11 @@
     };
     nvim-ufo = {
       enable = true;
+      luaConfig.post = ''
+        -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+        vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+        vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+      '';
       settings = {
         fold_virt_text_handler = ''
           function(virtText, lnum, endLnum, width, truncate)
