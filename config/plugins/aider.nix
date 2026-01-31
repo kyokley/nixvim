ebfbcf065c119addcc692c396107829c03bd2d06
Kevin Yokley <kyokley2@gmail.com>
2026-01-31 10:11:32 -0600

    WIP: Check for aider terminal window in keymap

--- config/plugins/aider.nix
+++ config/plugins/aider.nix
@@ -26,6 +26,17 @@
       args = { "--pretty", "--no-auto-commits", "--model=" .. (vim.env.NIXVIM_AIDER_MODEL or "ollama_chat/gpt-oss"), vim.env.NIXVIM_AIDER_EXTRA_ARGS or "" },
       auto_reload = true
     }
+
+    function strsplit(inputstr, sep)
+      if sep == nil then
+        sep = "%s"
+      end
+      local t = {}
+      for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        table.insert(t, str)
+      end
+      return t
+    end
   '';
 
   keymaps = [
@@ -83,6 +94,27 @@
       key = "<space>a+";
       action = ":AiderTreeAddFile<cr>";
       mode = ["n" "x"];
+    }
+    {
+      key = "jj";
+      # action = "<C-\\><C-n>";
+      action.__raw = ''
+        local win = vim.api.nvim_get_current_win()
+        local buf = vim.api.nvim_win_get_buf(win)
+
+        local buf_name = vim.api.nvim_buf_get_name(buf)
+        local cmd = strsplit(buf_name)[0]
+
+        if string.find(cmd, "aider") then
+          return "<C-\\><C-n>"
+        end
+
+        return "jj"
+      '';
+      mode = ["t"];
+      options = {
+        buffer = true;
+      };
     }
   ];
 }
