094eaf95172115a7958b6a4a194925f28ded9784
Kevin Yokley <yokley@mars>
2026-09-25 12:18:47 -0500

    fix(conform): correct formatter configuration
    - run Ruff import organization before Ruff formatting for Python
    - map shell files to shfmt
    - remove unused isort formatter definition

--- modules/conform.nix
+++ modules/conform.nix
@@ -15,20 +15,19 @@
           formatters_by_ft = {
             lua = ["stylua"];
             python = [
-              "ruff_format"
               "ruff_organize_imports"
+              "ruff_format"
             ];
             nix = ["alejandra"];
             html = ["htmlbeautifier"];
             htmldjango = ["djhtml"];
             json = ["jq"];
             javascript = ["prettier"];
-            bash = ["shfmt"];
+            sh = ["shfmt"];
             yaml = ["yamlfmt"];
           };
           formatters = {
             stylua.command = lib.getExe pkgs.stylua;
-            isort.command = lib.getExe pkgs.isort;
             ruff_format.command = lib.getExe pkgs.ruff;
             ruff_organize_imports.command = lib.getExe pkgs.ruff;
             alejandra.command = lib.getExe pkgs.alejandra;
